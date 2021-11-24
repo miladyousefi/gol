da90926feca42af3187e45e9be1d203c77b912e8
miladyousefi <yousefimiladdd@gmail.com>
2021-11-24 16:21:42 +0330

    upload level 3

---
+++ level_3/code/main.c
@@ -0,0 +1,35 @@
+/** \file main.c
+ * \brief programme s'exécutant dans une console :  
+ * charge une grille et débute le jeu
+ */
+
+#include <stdio.h>
+
+#include "grille.h"
+#include "io.h"
+#include "jeu.h"
+
+/** 
+ * \brief initialisation et début du jeu
+ * \param argc nombre d'arguments shell
+ * \param argv tableau des arguments shell
+ */
+int main (int argc, char ** argv) {
+	
+	if (argc != 2 )
+	{
+		printf("usage : main <fichier grille>");
+		return 1;
+	}
+
+	grille g, gc;
+	init_grille_from_file(argv[1],&g);
+	alloue_grille (g.nbl, g.nbc, &gc);
+	affiche_grille(g);
+	
+	debut_jeu(&g, &gc);
+
+	libere_grille(&g);
+	libere_grille(&gc);
+	return 0;
+}
